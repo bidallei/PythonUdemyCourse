72fa662bd094bd350fc7f935af0f2bdcdcd8e91f
bidallei <hjalvaro@gmailcom>
2024-11-18 20:42:02 -0600

    Avance en el curso

--- account/acc.py
+++ account/acc.py
@@ -15,9 +15,28 @@ class Account:
         with open(self.filepath, 'w') as file:
             file.write(str(self.balance))
 
+class Checking(Account):
 
-account = Account("balance.txt")
-print(account.balance)
-account.deposit(200)
-print(account.balance)
-account.commit()
+    """ This class generates checking account objetcs"""
+    type = "checking"
+
+    def __init__(self, filepath, fee):
+        Account.__init__(self, filepath)
+        self.fee = fee
+
+    def transfer(self, amount):
+        self.balance = self.balance - amount - self.fee
+
+jacks_checking = Checking("jack.txt", 1)
+jacks_checking.transfer(100)
+print(jacks_checking.balance)
+jacks_checking.commit()
+print(jacks_checking.type)
+
+johns_checking = Checking("john.txt", 1)
+johns_checking.transfer(100)
+print(johns_checking.balance)
+johns_checking.commit()
+print(johns_checking.type)
+
+print(johns_checking.__doc__)
